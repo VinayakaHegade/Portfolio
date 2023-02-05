e58507ebdcc304f5f8ca2a108d0fca872aafdbfe
Vinayak <myselfvinayakhegde@gmail.com>
2023-02-05 23:49:35 +0530

    Home section done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/vinayak-hegde-1492a71a1"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/VinayakaHegade"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://twitter.com/vinayakhegde8"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-twitter-alt"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social;

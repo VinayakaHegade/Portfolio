a6dda917101860d6361773f39c499cd895940824
Vinayak <myselfvinayakhegde@gmail.com>
2023-02-08 00:09:17 +0530

    contact structure done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Contact />
       </main>
     </>
   );

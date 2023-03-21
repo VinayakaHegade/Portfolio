e1c25797b1907fc744b0bf2eb4efd49098e26075
Vinayak <myselfvinayakhegde@gmail.com>
2023-03-21 20:10:05 +0530

    Added new project trello-board

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -1,8 +1,10 @@
 import Work1 from "../../assets/shopz.png";
 import Work2 from "../../assets/wordle.png";
-import Work3 from "../../assets/acehunter.png";
-import Work4 from "../../assets/tetris.png";
-import Work5 from "../../assets/portfolio.png";
+import Work3 from "../../assets/Trello.png";
+import Work4 from "../../assets/acehunter.png";
+import Work5 from "../../assets/tetris.png";
+import Work6 from "../../assets/portfolio.png";
+
 
 export const projectsData = [
   {
@@ -24,22 +26,30 @@ export const projectsData = [
   {
     id: 3,
     image: Work3,
+    title: "Trello Board",
+    category: "React",
+    demo: "https://trelloboard.vercel.app",
+    code: "https://github.com/VinayakaHegade/Trello-Board",
+  },
+  {
+    id: 4,
+    image: Work4,
     title: "Ace Hunter - Guess the ace",
     category: "Vanilla JS",
     demo: "https://acehunter.netlify.app",
     code: "https://github.com/VinayakaHegade/HuntTheAce",
   },
   {
-    id: 4,
-    image: Work4,
+    id: 5,
+    image: Work5,
     title: "Tetris",
     category: "Vanilla JS",
     demo: "https://play-tetris-game.netlify.app",
     code: "https://github.com/VinayakaHegade/tetris-game",
   },
   {
-    id: 5,
-    image: Work5,
+    id: 6,
+    image: Work6,
     title: "Portfolio",
     category: "React",
     demo: "https://vinayaka.netlify.app",

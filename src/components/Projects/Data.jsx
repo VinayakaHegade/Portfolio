9d949ba972fabfc5708e46f21290d6e1399767fa
Vinayak <myselfvinayakhegde@gmail.com>
2023-12-29 14:19:13 +0530

    feat: update projects

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -1,15 +1,17 @@
-import Work1 from "../../assets/shopz.png";
-import Work2 from "../../assets/wordle.png";
-import Work3 from "../../assets/Trello.png";
-import Work4 from "../../assets/acehunter.png";
-import Work5 from "../../assets/tetris.png";
-import Work6 from "../../assets/portfolio.png";
+import Shopz from "../../assets/shopz.png";
+import Wordle from "../../assets/wordle.png";
+import Trello from "../../assets/Trello.png";
+import AceHunter from "../../assets/acehunter.png";
+import Tetris from "../../assets/tetris.png";
+import Portfolio from "../../assets/portfolio.png";
+import Convio from "../../assets/Convio.png";
+import MovieSearch from "../../assets/movie-search.png";
 
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
+    image: Shopz,
     title: "Shopz - Ecommerce website",
     category: "React",
     demo: "https://ecomm-shopz.netlify.app",
@@ -17,39 +19,55 @@ export const projectsData = [
   },
   {
     id: 2,
-    image: Work2,
-    title: "Wordle - 5 letter word game",
+    image: Convio,
+    title: "Multimedia Converter",
     category: "React",
-    demo: "https://wordle-recreated.vercel.app",
-    code: "https://github.com/VinayakaHegade/React-Wordle",
+    demo: "https://convio.netlify.app",
+    code: "https://github.com/VinayakaHegade/file-converter",
   },
   {
     id: 3,
-    image: Work3,
+    image: MovieSearch,
+    title: "Movie Search App",
+    category: "React",
+    demo: "https://moviesearchtmdb.netlify.app",
+    code: "https://github.com/VinayakaHegade/Movie-Search",
+  },
+  {
+    id: 4,
+    image: Trello,
     title: "Trello Board",
     category: "React",
     demo: "https://trelloboard.vercel.app",
     code: "https://github.com/VinayakaHegade/Trello-Board",
   },
   {
-    id: 4,
-    image: Work4,
+    id: 5,
+    image: Wordle,
+    title: "Wordle - 5 letter word game",
+    category: "React",
+    demo: "https://wordle-recreated.vercel.app",
+    code: "https://github.com/VinayakaHegade/React-Wordle",
+  },
+  {
+    id: 6,
+    image: AceHunter,
     title: "Ace Hunter - Guess the ace",
     category: "Vanilla JS",
     demo: "https://acehunter.netlify.app",
     code: "https://github.com/VinayakaHegade/HuntTheAce",
   },
   {
-    id: 5,
-    image: Work5,
+    id: 7,
+    image: Tetris,
     title: "Tetris",
     category: "Vanilla JS",
     demo: "https://play-tetris-game.netlify.app",
     code: "https://github.com/VinayakaHegade/tetris-game",
   },
   {
-    id: 6,
-    image: Work6,
+    id: 8,
+    image: Portfolio,
     title: "Portfolio",
     category: "React",
     demo: "https://vinayaka.netlify.app",
